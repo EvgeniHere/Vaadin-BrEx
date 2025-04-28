89c686ef02efac1620264db360512b3d0b1e3f5c
exobase <exobaserl@gmail.com>
2025-04-28 21:05:41 +0200

    Init project

---
+++ Application/src/main/java/io/exo/base/domain/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable domain classes.
+ */
+@NullMarked
+package io.exo.base.domain;
+
+import org.jspecify.annotations.NullMarked;
---
+++ Application/src/main/java/io/exo/base/ui/component/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable UI components.
+ */
+@NullMarked
+package io.exo.base.ui.component;
+
+import org.jspecify.annotations.NullMarked;
